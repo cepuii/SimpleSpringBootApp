f93b36792a45ab0341866c0afa72917116961c9b
cepuii <cepuii13@gmail.com>
2022-06-15 00:14:00 +0300

    Create gradle build files
    Add first classes, and first dependencies

---
+++ src/test/java/edu/cepuii/spring_gradle_docker_work/SpringGradleDockerWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cepuii.spring_gradle_docker_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleDockerWorkApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
