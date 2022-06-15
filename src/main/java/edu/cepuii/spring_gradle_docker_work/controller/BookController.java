66f7a8ff631f9b594bc603e07a9c16a796be0ef3
cepuii <cepuii13@gmail.com>
2022-06-15 19:35:07 +0300

    Add controller /books

---
+++ src/main/java/edu/cepuii/spring_gradle_docker_work/controller/BookController.java
@@ -0,0 +1,39 @@
+package edu.cepuii.spring_gradle_docker_work.controller;
+
+import edu.cepuii.spring_gradle_docker_work.dto.BookRequest;
+import edu.cepuii.spring_gradle_docker_work.model.Book;
+import edu.cepuii.spring_gradle_docker_work.service.BookService;
+import edu.cepuii.spring_gradle_docker_work.service.BookToDtoMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+  
+  private BookService service;
+  private BookToDtoMapper mapper;
+  
+  @GetMapping("/{id}")
+  public Book getBookById(@PathVariable long id) {
+    return service.getBookById(id);
+  }
+  
+  @GetMapping
+  public List<Book> getAllBooks() {
+    return service.getAllBooks();
+  }
+  
+  @PostMapping
+  public void addBook(@RequestBody BookRequest bookRequest) {
+    service.addBook(mapper.bookRequestToBook(bookRequest));
+  }
+  
+}
