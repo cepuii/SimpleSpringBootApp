a484cc32579d0db702c323fd4ff0a44879e7c56d
cepuii <cepuii13@gmail.com>
2022-06-16 23:02:57 +0300

    Comment test file

--- src/test/java/edu/cepuii/spring_gradle_docker_work/SpringGradleDockerWorkApplicationTests.java
+++ src/test/java/edu/cepuii/spring_gradle_docker_work/SpringGradleDockerWorkApplicationTests.java
@@ -1,13 +1,13 @@
-package edu.cepuii.spring_gradle_docker_work;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringGradleDockerWorkApplicationTests {
-  
-  @Test
-  void contextLoads() {
-  }
-  
-}
+//package edu.cepuii.spring_gradle_docker_work;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringGradleDockerWorkApplicationTests {
+//
+//  @Test
+//  void contextLoads() {
+//  }
+//
+//}
